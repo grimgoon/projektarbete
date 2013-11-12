78cc245a1c25175471b0e652e3c0aa77f94e9ee8
GrimGoon <grimgoon@donorsbyheart.com>
2013-11-12 14:16:03 +0100

    Added the project

---
+++ Assets/project/Menu/mainMenu/GUIMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUIMenu : MonoBehaviour 
+{
+
+    public GUISkin mSkin; // Används för att lägga till GUI-Skinet "Fantasy Buttons GUI" till prefaben "GUIMenu".
+	public bool buttonPlay; // registrerar om knappen "play" blir tryckt vilket ger värdet true som används för att koppla en handling till knappen.
+	public bool buttonOptions; // - || - 
+	public bool buttonExit;  // - || -
+	public float native_width = 1920;
+	public float native_height = 1080;
+	
+	    public void OnGUI()
+    {
+        if (mSkin != null) // Om inget GUI-Skin är valt så använder det ett default skin som är inbyggt i Unity. 
+        {
+            GUI.skin = mSkin;
+        }
+		
+		
+		float rx = Screen.width / native_width;
+    	float ry = Screen.height / native_height;
+    	GUI.matrix = Matrix4x4.TRS (Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, new Vector3 (rx, ry, 1.0f)); 
+		
+		GUILayout.BeginArea( new Rect((Screen.width/2)-192, (Screen.height/2)-80, 512, 400)); // Väljer storlek & Position - (X,Y,bredd, höjd);
+			GUILayout.BeginVertical("window"); // Startar en vertikal kontrollgrupp, "Window" avser vilket del av skinet som skall användas för kontrollgruppen
+				buttonPlay = GUILayout.Button("Play"); // Knapp
+				GUILayout.FlexibleSpace(); //Flexible space skapar ett mellanrum som är beroende på hur mycket utrymme som finns över utav rektangelen (Rect) som man ritat upp.
+				buttonOptions = GUILayout.Button("Options"); //Knapp
+				GUILayout.FlexibleSpace(); // Mellanrum
+				buttonExit = GUILayout.Button ("Exit"); //Knapp
+			GUILayout.EndVertical(); // Avslutar den vertikala kontrollgruppen
+        GUILayout.EndArea(); // Avslutar Området
+		
+		
+		if (buttonPlay == true) // Vad som sker om man trycker på knappen button "Play".
+			Application.LoadLevel("playerhouse"); // Skickar en till scenen "playerhouse"
+		
+		if (buttonOptions == true) // -||-
+			Application.LoadLevel("menuOptions"); // skickar en till scenen "menuOptions"
+		
+		if (buttonExit == true) // -||-
+			Application.Quit(); // Stänger av programmet.
+    }
+}
+
---
+++ Assets/project/Menu/optionsMenu/GUIMenuOptions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class GUIMenuOptions : MonoBehaviour 
+{
+
+    public GUISkin mSkin; // Används för att lägga till GUI-Skinet "Fantasy Buttons GUI" till prefaben "GUIMenu".
+	public bool buttonBack;  // // registrerar om knappen "Back" blir tryckt vilket ger värdet true som används för att koppla en handling till knappen. 
+	static float WIDTH = 1920;
+	static float Height = 1080;
+	    public void OnGUI()
+    {
+        if (mSkin != null) // Om inget GUI-Skin är valt så använder det ett default skin som är inbyggt i Unity. 
+        {
+            GUI.skin = mSkin;
+        }
+		
+		GUILayout.BeginArea( new Rect((Screen.width/2)-192, (Screen.height/2)-80, 0.26f * Screen.width, 0.37f * Screen.height)); // Väljer storlek & Position - (X,Y,bredd, höjd);
+			GUILayout.BeginVertical("window"); // Startar en vertikal kontrollgrupp, "Window" avser vilket del av skinet som skall användas för kontrollgruppen
+				GUILayout.FlexibleSpace(); //Flexible space skapar ett mellanrum som är beroende på hur mycket utrymme som finns över utav rektangelen (Rect) som man ritat upp.
+				buttonBack = GUILayout.Button("Back"); //Knapp
+			GUILayout.EndVertical(); // Avslutar den vertikala kontrollgruppen
+        GUILayout.EndArea(); // Avslutar Området
+		
+		
+		if (buttonBack == true) // Vad som sker om man trycker på knappen "Back".
+			Application.LoadLevel("menu"); // Skickar en tillbaka till huvudmenyn.
+		
+    }
+}
+
